1213c73568e53e74e6f20e899b3c5fb525ae540e
clementp0 <clement.pommerie@gmail.com>
2024-08-13 20:20:54 +0200

    Feat : Sass pre-loader, icons & home

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,17 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import HomePage from './components/HomePage.vue'
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    <a href="https://a2urbex.eu" target="_blank">
+      <img src="/logox192.png" class="logo" alt="a2urbex logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HomePage msg="a2urbex - Your exploration co-pilot" />
+  <p class="test">Check out our repository on <a href="https://github.com/a2urbex" target="_blank">Github</a></p>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style lang="scss" scoped>
+  @import './assets/homepage.scss';
 </style>

1ef28ab1ede53444da29482ab3aca25d910814a0
Clement Po <clement.pommerie@gmail.com>
2025-11-19 21:51:58 +0100

    chore: Bump version to 0.0.84 and refine styling for location cards and map components.

--- src/stores/version.js
+++ src/stores/version.js
@@ -3,7 +3,7 @@ import { defineStore } from 'pinia';
 export const useVersionStore = defineStore('version', {
     
     state: () => ({
-        code_version: '0.0.83', // Use for dev only
+        code_version: '0.0.84', // Use for dev only
         latest_version: ''
     }),
     actions: {
